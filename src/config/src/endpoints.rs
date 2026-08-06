fe8abb539a1e2a62d90e092d11a913b4a6218356
Griffin Pundt <116926805+gpundt@users.noreply.github.com>
2026-08-06 15:59:25 -0400

    refactored filpath declarations, adding more endpoints

--- src/config/src/endpoints.rs
+++ src/config/src/endpoints.rs
@@ -7,3 +7,10 @@ pub const QUERY_ENDPOINT: &str = formatcp!("{}/query", API_ENDPOINT);
 pub const HEALTH_CHECK_ENDPOINT: &str = formatcp!("{}/health_check", QUERY_ENDPOINT);
 pub const CONFIG_QUERY_ENDPOINT: &str = formatcp!("{}/config", QUERY_ENDPOINT);
 pub const STATUS_QUERY_ENDPOINT: &str = formatcp!("{}/status", QUERY_ENDPOINT);
+
+pub const SUBMISSION_ENDPOINT: &str = formatcp!("{}/submit", API_ENDPOINT);
+pub const SUBMIT_CONFIG_ENDPOINT: &str = formatcp!("{}/config", SUBMISSION_ENDPOINT);
+pub const SUBMIT_TASK_ENDPOINT: &str = formatcp!("{}/task", SUBMISSION_ENDPOINT);
+
+pub const TRANSMISSION_ENDPOINT: &str = formatcp!("{}/transmit", API_ENDPOINT);
+pub const TRANSMIT_TEXT_ENDPOINT: &str = formatcp!("{}/text", TRANSMISSION_ENDPOINT);
--- src/config/src/files.rs
+++ src/config/src/files.rs
@@ -2,34 +2,29 @@ use std::fs;
 
 // ──── Linux ─────────────────────────────────────────────────────────────
 #[cfg(not(target_os = "windows"))]
-pub const ETC_DIR: &str = "/etc/outpost";
-#[cfg(not(target_os = "windows"))]
-pub const CONFIG_FILE: &str = "/etc/outpost/outpost.conf";
+mod platform {
+    pub const ETC_DIR: &str = "/etc/outpost";
+    pub const CONFIG_FILE: &str = "/etc/outpost/outpost.conf";
 
-#[cfg(not(target_os = "windows"))]
-pub const OPT_DIR: &str = "/opt/outpost";
-#[cfg(not(target_os = "windows"))]
-pub const LOG_DIR: &str = "/opt/outpost/logs";
-#[cfg(not(target_os = "windows"))]
-pub const BIN_DIR: &str = "/opt/outpost/bin";
-#[cfg(not(target_os = "windows"))]
-pub const TLS_DIR: &str = "/opt/outpost/tls";
+    pub const OPT_DIR: &str = "/opt/outpost";
+    pub const LOG_DIR: &str = "/opt/outpost/logs";
+    pub const BIN_DIR: &str = "/opt/outpost/bin";
+    pub const TLS_DIR: &str = "/opt/outpost/tls";
+}
 
 // ──── Windows ─────────────────────────────────────────────────────────────
 #[cfg(target_os = "windows")]
-pub const ETC_DIR: &str = "C:\\Users\\Public\\etc";
-#[cfg(target_os = "windows")]
-pub const CONFIG_FILE: &str = "C:\\Users\\Public\\etc\\outpost.conf";
+mod platform {
+    pub const ETC_DIR: &str = "C:\\Users\\Public\\etc";
+    pub const CONFIG_FILE: &str = "C:\\Users\\Public\\etc\\outpost.conf";
 
-#[cfg(target_os = "windows")]
-pub const OPT_DIR: &str = "C:\\Users\\Public\\opt";
-#[cfg(target_os = "windows")]
-pub const LOG_DIR: &str = "C:\\Users\\Public\\logs";
-#[cfg(target_os = "windows")]
-pub const BIN_DIR: &str = "C:\\Users\\Public\\bin";
-#[cfg(target_os = "windows")]
-pub const TLS_DIR: &str = "C:\\Users\\Public\\tls";
+    pub const OPT_DIR: &str = "C:\\Users\\Public\\opt";
+    pub const LOG_DIR: &str = "C:\\Users\\Public\\logs";
+    pub const BIN_DIR: &str = "C:\\Users\\Public\\bin";
+    pub const TLS_DIR: &str = "C:\\Users\\Public\\tls";
+}
 
+pub use platform::*;
 pub fn create_output_directories() -> Result<(), Box<dyn std::error::Error>> {
     fs::create_dir_all(ETC_DIR)?;
     fs::create_dir_all(LOG_DIR)?;
