674ffe22326d3628b2fe79afd73208d23b8c61b6
Griffin Pundt <116926805+gpundt@users.noreply.github.com>
2026-08-11 08:21:01 -0400

    easy db select functions (NOT TESTED YET)

--- src/outpost_server/src/database/select.rs
+++ src/outpost_server/src/database/select.rs
@@ -1,17 +1,65 @@
 use super::schema::{
     MeshtasticNodeEntry, MeshtasticPositionEntry, MeshtasticRawEntry, MeshtasticTextEntry,
+    get_db_pool,
 };
+use log::error;
 
-// pub async fn select_meshtastic_texts_by_count(
-//     count: u32,
-// ) -> Result<Vec<MeshtasticTextEntry>, sqlx::Error> {
-// }
+pub async fn select_meshtastic_texts_by_count(
+    count: u32,
+) -> Result<Vec<MeshtasticTextEntry>, sqlx::Error> {
+    match sqlx::query_as::<_, MeshtasticTextEntry>(
+        "SELECT id, timestamp, src_id, dst_id, message FROM meshtastic_texts ORDER BY id DESC LIMIT ?",
+    ).bind(count).fetch_all(get_db_pool()).await {
+        Ok(texts) => {
+            return Ok(texts);
+        },
+        Err(e) => {
+            error!("Failed to query {} texts from 'meshtastic_texts' table: {}", count, e);
+            return Err(e);
+        }
+    };
+}
 
-// pub async fn select_meshtastic_nodes() -> Result<Vec<MeshtasticNodeEntry>, sqlx::Error> {}
+pub async fn select_meshtastic_nodes() -> Result<Vec<MeshtasticNodeEntry>, sqlx::Error> {
+    match sqlx::query_as::<_, MeshtasticNodeEntry>(
+        "SELECT id, node_num, node_id, node_long_name, node_short_name, hw_model, role, last_heard, uptime, channel, hops_away FROM meshtastic_nodes",
+    ).fetch_all(get_db_pool()).await {
+        Ok(nodes) => {
+            return Ok(nodes);
+        },
+        Err(e) => {
+            error!("Failed to query nodes from 'meshtastic_nodes' table: {}", e);
+            return Err(e);
+        }
+    };
+}
 
-// pub async fn select_meshtastic_positions() -> Result<Vec<MeshtasticPositionEntry>, sqlx::Error> {}
+pub async fn select_meshtastic_positions() -> Result<Vec<MeshtasticPositionEntry>, sqlx::Error> {
+    match sqlx::query_as::<_, MeshtasticPositionEntry>(
+        "SELECT id, latitude, longitude, altitude, time, timestamp, next_update FROM meshtastic_positions",
+    ).fetch_all(get_db_pool()).await {
+        Ok(positions) => {
+            return Ok(positions);
+        }
+        Err(e) => {
+            error!("Failed to query positions from 'meshtastic_positions' table: {}", e);
+            return Err(e);
+        }
+    }
+}
 
-// pub async fn select_meshtastic_raw_by_count(
-//     count: u32,
-// ) -> Result<Vec<MeshtasticRawEntry>, sqlx::Error> {
-// }
+pub async fn select_meshtastic_raw_by_count(
+    count: u32,
+) -> Result<Vec<MeshtasticRawEntry>, sqlx::Error> {
+    match sqlx::query_as::<_, MeshtasticRawEntry>(
+        "SELECT id, src_node, dst_node, channel, hop_limit, hop_start, hop_next, encrypted FROM meshtastic_raw ORDER BY id DESC LIMIT ?",
+    ).bind(count).fetch_all(get_db_pool()).await {
+        Ok(raw) => {
+            return Ok(raw);
+        }
+        Err(e) => {
+            error!("Failed to query {} raw packets from 'meshtastic_raw' table: {}", count, e);
+            return Err(e);
+        }
+    }
+}
