6ddc80ca4deef7882ce0b322857635a94acea93d
Griffin Pundt <116926805+gpundt@users.noreply.github.com>
2026-08-13 12:52:26 -0400

    working on client!

---
+++ src/outpost_client/src/arguments.rs
@@ -0,0 +1,39 @@
+use std::sync::OnceLock;
+
+use clap::Parser;
+
+/// All available CLI arguments
+#[derive(Parser, Debug)]
+#[command(name = "Outpost Client")]
+#[command(version = "0.1.0")]
+#[command(about = "Meshtastic Node Query Client")]
+pub struct Args {
+    /// Enables debug log output
+    #[arg(short, long, default_value_t = false)]
+    pub debug: bool,
+
+    /// Port to bind HTTP listener to
+    #[arg(long, default_value_t = 8080)]
+    pub http_port: u16,
+
+    #[arg(short, long, default_value_t = "192.168.99.1".to_string())]
+    pub server_ip: String,
+
+    #[arg(short, long, default_value_t = false)]
+    pub test: bool,
+}
+
+// static global storage
+static ARGS: OnceLock<Args> = OnceLock::new();
+
+/// Populates globally-accessible ARGS static
+pub fn initialize_arguments() {
+    let args = Args::parse();
+    ARGS.set(args)
+        .expect("Failed to set global arguments storage")
+}
+
+/// Used to access the globally-accessible ARGS struct
+pub fn get_arguments() -> &'static Args {
+    ARGS.get().expect("Arguments not initialized yet")
+}
--- src/outpost_client/src/main.rs
+++ src/outpost_client/src/main.rs
@@ -1,5 +1,30 @@
+mod arguments;
+
 pub mod http;
+use arguments::{get_arguments, initialize_arguments};
+
+use config::{files::create_output_directories, logging::initialize_logger, time::start_time};
 
 fn main() {
-    println!("Hello, world!");
+    start_time();
+    initialize_arguments();
+
+    if get_arguments().test {
+        return;
+    }
+
+    match create_output_directories("client") {
+        Ok(_) => {}
+        Err(e) => {
+            println!("{}", e.to_string());
+            return;
+        }
+    }
+    match initialize_logger("client", get_arguments().debug) {
+        Ok(_) => {}
+        Err(e) => {
+            println!("{}", e.to_string());
+            return;
+        }
+    };
 }
